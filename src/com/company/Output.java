20ce92eaac06699d535322614b6ce4ffaf161809
secminhr <secminhrian@gmail.com>
2022-11-16 23:23:43 +0800

    Fix Output implementation

--- src/com/company/Output.java
+++ src/com/company/Output.java
@@ -66,12 +66,12 @@ public class Output {
     }
 
     public void individual_opponent_card(PlayerStatus clientPlayer){
-        System.out.println(clientPlayer.getCard().toString());
+        output.println(clientPlayer.getCard().toString());
     }
 
     public void broadcast_the_card_be_discard(PlayerStatus playerStatus){
-        System.out.println();
-        System.out.println(" it is the card which is discarded : " + playerStatus.getCard());
-        System.out.println();
+        output.println();
+        output.println(" it is the card which is discarded : " + playerStatus.getCard());
+        output.println();
     }
 }
