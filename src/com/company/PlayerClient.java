5b27c2eab16997e319c3f9b8db56c98664dccd84
secminhr <secminhrian@gmail.com>
2022-11-24 17:16:25 +0800

    simplify connection

--- src/com/company/PlayerClient.java
+++ src/com/company/PlayerClient.java
@@ -25,8 +25,6 @@ public class PlayerClient {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Room host ip: ");
         String hostIp = scanner.next();
-        System.out.print("My ip: ");
-        String myIp = scanner.next();
         System.out.print("room number: ");
         int roomNumber = scanner.nextInt();
 
@@ -37,9 +35,8 @@ public class PlayerClient {
 
         int id = host.getId();
 
-        registry = LocateRegistry.createRegistry(DEFAULT_PORT);
         registry.rebind(String.format("%d-%d", roomNumber, id), UnicastRemoteObject.exportObject(client, 0));
 
-        host.enter(myIp, id);
+        host.enter(id);
     }
 }
